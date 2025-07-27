132fff4a54a83f39cdca09fc6dd699fe3e00975a
John Lemar Gonzales <johnlemargonzales@gmail.com>
2025-07-28 04:27:21 +0800

    Add .next/static/chunks/src_app_favorites_page_tsx_10c7cf24._.js 1753648041784

---
+++ .next/static/chunks/src_app_favorites_page_tsx_10c7cf24._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_bda42680._.js",
+  "static/chunks/src_ef55a279._.js"
+],
+    source: "dynamic"
+});
